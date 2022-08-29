ee279a6b96a02a1b99758844f8d10a2b37e7a635
nathanielzacharias <nathanielzacharias@gmail.com>
2022-08-29 17:05:52 +0800

    feat: added Routes, Route and Link to App.js, all working, homework completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,37 @@
 import React from "react";
 import Home from "./Home";
+import Contact from "./Contact"
+import Procedures from "./Procedures"
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 const App = () => {
+
     return (
         <div>
           <nav>
-            <a href="/">Go to Home Page</a>
+            {/* <a href="/">Go to Home Page</a>
             <a href="/procedures">See Our Procedures</a>
-            <a href="/contact">Contact Us!</a>
+            <a href="/contact">Contact Us!</a> */}
+            
+            <Link to="/">Go to Home Page</Link>
+            <Link to="/procedures">See Our Procedures</Link>
+            <Link to="/contact">Contact Us!</Link>
+
           </nav>
+
           <div>
-            <Home />
+            {/* <Home /> */}
+
+            <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/procedures" element={<Procedures/>} />
+              <Route path="/contact" element={<Contact/>} />
+            </Routes>
+
           </div>
         </div>
     );
