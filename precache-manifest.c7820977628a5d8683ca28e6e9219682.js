1edc3cce7da53bbf8b5e82e4588c72b34f9d2e36
nathanielzacharias <nathanielzacharias@gmail.com>
2022-08-29 17:27:27 +0800

    Updates

---
+++ precache-manifest.c7820977628a5d8683ca28e6e9219682.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7978f83b2b029fcb8ec5aeaa23401946",
+    "url": "/dentist_homework/index.html"
+  },
+  {
+    "revision": "83a209077b1b53162443",
+    "url": "/dentist_homework/static/css/main.3be36971.chunk.css"
+  },
+  {
+    "revision": "b04ca73ab78ec7cd4ad0",
+    "url": "/dentist_homework/static/js/2.7ef4ad68.chunk.js"
+  },
+  {
+    "revision": "83a209077b1b53162443",
+    "url": "/dentist_homework/static/js/main.8e1531e8.chunk.js"
+  },
+  {
+    "revision": "7f7da2922060b5ab8f8b",
+    "url": "/dentist_homework/static/js/runtime~main.c93b112c.js"
+  }
+]);
